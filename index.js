6e99bdcfc866648375ef3d171d584b018457af32
LAPTOP-KO3E8U1N\18082 <ZakharyOliver808@gmail.com>
2021-12-21 00:46:22 -1000

    updates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import http from "http";
 import url from "url";
+import {CTjson, CThtml, AC, RC, SendResponse} from "./lib/helpers.js"
 import { verifyId } from "./googleapis/gmail/service.js";
 import { listEvents } from "./googleapis/googlecalendar/index.js";
 import { mapGithubData } from "./webscrappers/github/service.js";
@@ -11,91 +12,6 @@ const init = async () => {
   const PORT = 3200;
   server.on("request", (request, response) => {
     const parsedUrl = url.parse(request.url, true);
-
-    /**
-    response.setHeader("Content-Type", "application/json")
-    @abstract CTjson
-    */
-    const CTjson = response.setHeader("Content-Type", "application/json");
-
-
-    /** 
-    response.setHeader("Content-Type", "text/html", "charset=utf-8") 
-    @abstract CThtml 
-    */
-    const CThtml = response.setHeader("Content-Type", "text/html", "charset=utf-8")
-
-
-    /** 
-    const AC = response.setHeader("Access-Control-Allow-Origin", "*")
-    @abstract AC 
-    */
-    const AC = response.setHeader("Access-Control-Allow-Origin", "*")
-
-
-
-    /**
-    @function 
-    const RC = (code) => response.statusCode = code;
-    @description
-    Sets a status code response
-    @param code
-    The status code to return
-    */
-    const RC = (code) => response.statusCode = code;
-
-
-    /**
-    @function
-    const SendResponse = (Function, DataPoints = [], response) => {
-      let responseData = [];
-      let timeout = setTimeout(() => {
-        response.write(JSON.stringify(responseData));
-        response.end();
-      }, 10000);
-      let Args = DataPoints.map((point) => {
-        return (pointVar) => {
-          let Object = `${point}: ${pointVar}`
-          responseData.push({ Object });
-          if (responseData.length == DataPoints.length) {
-            clearTimeout(timeout);
-            response.write(JSON.stringify(responseData));
-            response.end();
-          }
-        }
-      })
-      Function(...Args)
-    }
-    @description
-    Calls a backend service that retrieves data from a 3rd party
-    @param Function
-    The backend service function to call
-    @param DataPoints
-    An array of Data Point arguments accepted by the back end services
-    */
-    const SendResponse = (Function, DataPoints = []) => {
-      let responseData = [];
-      let timeout = setTimeout(() => {
-        response.write(JSON.stringify(responseData));
-        response.end();
-      }, 10000);
-      let Args = DataPoints.map((point) => {
-        return (pointVar) => {
-          let data = Object.create({})
-          data[`${point}`] = pointVar
-          responseData.push(data);
-          if (responseData.length == DataPoints.length) {
-            clearTimeout(timeout);
-            response.write(JSON.stringify(responseData));
-            response.end();
-          }
-        }
-      })
-      Function(...Args)
-    }
-
-
-
     console.log(request.method);
     console.log(parsedUrl.pathname);
     try {

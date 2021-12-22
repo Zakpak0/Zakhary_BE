e5695bcc116b1506fd2d94f014a912a8c1e7c792
Zakpak0 <ZakharyOliver808@gmail.com>
2021-12-22 05:43:57 -1000

    Live api

---
+++ src/templates/Homepage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const Homepage = () => {
+    return (
+        
+        <div style={{
+            padding: "10px 10px 10px 10px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <h1 style={{
+                border: "solid",
+                borderWidth: "0.005rem",
+                
+            }} >API Zakpak</h1>
+        </div>
+    )
+}
+export default Homepage

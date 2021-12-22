1940cb5b12f9ffcb1b159c992e19ce577b9b629e
Zakpak0 <ZakharyOliver808@gmail.com>
2021-12-22 04:55:20 -1000

    again

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     target: 'node',
     entry: "./src/index.js",
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname),
         filename: "index.js"
     },
     module: {
